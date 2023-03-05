b856dee205bfed2901af2f2c64324296f09b4f49
Daniel <kronkowski.daniel@gmail.com>
2023-03-05 16:04:38 +0100

    add Paddle draw

--- gameback/paddle.py
+++ gameback/paddle.py
@@ -1,25 +1,22 @@
 import pygame
 
 class Paddle(pygame.sprite.Sprite):
-    def __init__(self, x, y, width, height, color):
+    def __init__(self, width, height, color):
         super().__init__()
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
         self.color = color
         self.speed = 10
 
-        self.image = pygame.Surface([self.width, self.width])
+
+        self.image = pygame.Surface((width, height))
         self.image.fill((0,0,0))
         self.image.set_colorkey((0,0,0))
 
-        pygame.draw.rect(self.image, self.color, (self.x, self.y, self.width, self.height))
+        pygame.draw.rect(self.image, self.color, (0, 0, width, height))
         self.rect = self.image.get_rect()
 
-    def player_input(self):
+    '''def player_input(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT] and self.x >= 10:
             self.x -= self.speed 
         if keys[pygame.K_RIGHT] and self.x <=790:
-            self.x += self.speed
+            self.x += self.speed'''
--- main.py
+++ main.py
@@ -9,7 +9,18 @@ pygame.init()
 screen = pygame.display.set_mode((800,600))
 pygame.display.set_caption('Brick breaker')
 run = True 
+clock = pygame.time.Clock()
+font = pygame.font.Font(None,34)
 
+WHITE = (255,255,255)
+DARKBLUE = (36,90,190)
+LIGHTBLUE = (0,176,240)
+RED = (255,0,0)
+ORANGE = (255,100,0)
+YELLOW = (255,255,0)
+ 
+score = 0
+lives = 3
 
 ball = Ball(400, 300, 10, (255, 0,0,))
 bricks = []
@@ -19,9 +30,17 @@ for i in range (10):
     bricks.append(brick)
 
 # Groups
-paddle = pygame.sprite.GroupSingle()
-paddle.add(Paddle(350, 580, 100, 10, (0,0,255)))
+'''paddle = pygame.sprite.GroupSingle()
+paddle.add(Paddle(350, 580, 100, 10, LIGHTBLUE))'''
 
+object_group = pygame.sprite.Group()
+paddle = Paddle(100, 10,LIGHTBLUE)
+paddle.rect.x = 350
+paddle.rect.y = 560
+
+#Add to group
+
+object_group.add(paddle)
 
 while run:
     
@@ -29,9 +48,19 @@ while run:
         if event.type == pygame.QUIT:
             run = False 
 
-    screen.fill((255,255,255))
-    paddle.draw(screen)
-    paddle.update()
+    
+    object_group.update()
+
+    screen.fill((DARKBLUE)) #
+    pygame.draw.line(screen,WHITE, [0,38],[800,38],2) # rysowanie lini
+
+    text_score = font.render("Score:" + str(score), 1, WHITE)
+    screen.blit(text_score,(20,10))
+
+    text_lives = font.render("Lives:" + str(lives), 1, WHITE)
+    screen.blit(text_lives,(650,10))
+    
+    object_group.draw(screen)
 
     pygame.display.update()
 
